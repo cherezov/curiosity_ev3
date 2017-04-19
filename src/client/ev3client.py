9e05c8db86cad98c4ffb3c5ac5271ef5d5575274
Pavel Cherezov <pavelc@cqg.com>
2017-04-19 12:49:18 +0300

    Initial folders and files added

---
+++ src/client/ev3client.py
@@ -0,0 +1,4 @@
+# @file ev3client.py
+# @author Pavel Cherezov (cherezov.pavel@gmail.com)
+
+print('TODO: implement EV3 client.')
---
+++ src/ev3server/ev3server.py
@@ -0,0 +1,4 @@
+# @file ev3server.py
+# @author Pavel Cherezov (cherezov.pavel@gmail.com)
+
+print('TODO: implement EV3 listening server.')
